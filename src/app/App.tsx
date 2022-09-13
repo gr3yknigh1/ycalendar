890698f3a6149770f39f5d942096fcbe46b9a200
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-13 13:56:01 +0300

    Add anouter routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,10 @@ import store from './store';
 
 import {
   HomePage,
+  AboutPage,
+  CalendarPage,
+  SettingsPage,
+  NotFoundPage,
 } from '@/routes';
 
 
@@ -18,6 +22,10 @@ export default function App(): JSX.Element {
     <Provider store={ store }>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Provider>
   );
