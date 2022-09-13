5baa5a8ac49e2576b84adc92b99bedc7d017ee50
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-13 13:55:03 +0300

    Add basic routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,24 @@
 import React, { useCallback } from 'react';
 import { Provider } from 'react-redux';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 import store from './store';
+
+import {
+  HomePage,
+} from '@/routes';
+
 
 export default function App(): JSX.Element {
 
   return (
     <Provider store={ store }>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Provider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/app';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App/>
+  <StrictMode>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </StrictMode>
 );
