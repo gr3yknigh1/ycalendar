8989d926e87020d16b4f99bd57636e3cb9d63077
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-16 23:07:35 +0300

    Remove entry point from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,2 @@
-import './index.css';
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from '@/app';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </StrictMode>
-);
+export {
+};
