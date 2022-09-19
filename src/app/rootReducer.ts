3763897720aef29a9fefe7a3ab6b129950091c70
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-19 14:25:45 +0300

    Include task's reducer to root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { calendarReducer } from '@/features/calendar';
+import { tasksReducer } from '@/features/tasks';
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
+  tasks: tasksReducer,
 });
 
 export default rootReducer;
